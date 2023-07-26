73d5b0662748ffcbb286c5dee7e0340ce8d6f729
Rodrigo Laviani <rodrigo.laviani@naranja.com>
2023-07-26 11:28:03 -0300

    feat: services and endpoints

--- src/routes/pet.routes.ts
+++ src/routes/pet.routes.ts
@@ -1,34 +1,32 @@
 import { Router } from 'express'
-import { PetController } from '../controllers/pet.controller';
-
-const petController = new PetController();
+import { default as PetController } from '../controllers/pet.controller';
 
 const router = Router();
 
 /** routes */
 // Update an existing pet
-router.route('').put(petController.updatePet);
+router.route('').put(PetController.updatePet);
 
 // Add a new pet to the store
-router.route('').post(petController.addPet);
+router.route('').post(PetController.addPet);
 
 // Finds Pets by status
-router.route('/findByStatus').get(petController.findPetByStatus);
+router.route('/findByStatus').get(PetController.findPetByStatus);
 
 // Finds Pets by tags
-router.route('/findByTags').get(petController.findPetByTags);
+router.route('/findByTags').get(PetController.findPetByTags);
 
 // Find pet by ID
-router.route('/:petId').get(petController.findPetById);
+router.route('/:petId').get(PetController.findPetById);
 
 // Updates a pet in the store with form data
-router.route('/:petId').post(petController.updatePetNameAndStatus);
+router.route('/:petId').post(PetController.updatePetNameAndStatus);
 
 // Deletes a pet
-router.route('/:petId').delete(petController.deletePet);
+router.route('/:petId').delete(PetController.deletePet);
 
 // Uploads an image
-router.route('/:petId/uploadImage').post(petController.uploadImage);
+router.route('/:petId/uploadImage').post(PetController.uploadImage);
 
 
 export default router;
